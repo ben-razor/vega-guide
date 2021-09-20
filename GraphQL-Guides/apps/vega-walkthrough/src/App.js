f4a9c802de768b5c044dd7569719935ea10bff06
Ben Razor <chrisbenwar1@googlemail.com>
2021-09-20 12:28:39 +0100

    Add markdown display to walkthrough app

--- GraphQL-Guides/apps/vega-walkthrough/src/App.js
+++ GraphQL-Guides/apps/vega-walkthrough/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 import { useQuery, gql } from "@apollo/client";
 import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror';
@@ -11,46 +12,85 @@ import 'codemirror-graphql/hint';
 import 'codemirror-graphql/lint';
 import 'codemirror-graphql/mode';
 
-const options = {
+const editorOptions = {
   lineNumbers: true,
   readOnly: false,
   mode: 'graphql',
   autofocus: true,
   smartIndent: false,
-  tabSize: 2
+  tabSize: 2,
+  lineWrapping: true
 }
 
 function App() {
   const [value, setValue] = useState();
-  console.log(value);
+  const [markdown, setMarkdown] = useState();
+  const [sectionID, setSectionID] = useState('introduction');
+
+  function setInstructions(text) {
+    console.log(text);
+    setMarkdown(text);
+  }
+
+  useEffect(() => {
+    let mdFileName = `${sectionID}.md`;
+    import(`./markdown/${mdFileName}`)
+    .then(res => {
+        fetch(res.default)
+            .then(res => res.text())
+            .then(res => setInstructions(res))
+            .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+
+    let gqlFileName = `${sectionID}.gql`;
+    import(`./graphql/${gqlFileName}`)
+    .then(res => {
+        fetch(res.default)
+            .then(res => res.text())
+            .then(res => setValue(res))
+            .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+
+
+    
+  }, [sectionID])
+
   return (
     <div className="App">
-      <div class="walkthrough-header">
+      <div className="walkthrough-header">
         <h3>
           Vega Protocol GraphQL Walkthrough
         </h3>
       </div>
-      <div class="walkthrough-panels">
-        <div class="walkthrough-panel walkthrough-panels-tutorial">
-          <div class="walkthrough-controls">
-            <button class="walkthrough-control-button"><i class="fa fa-arrow-left" /></button>
+      <div className="walkthrough-panels">
+        <div className="walkthrough-panel walkthrough-panels-tutorial">
+          <div className="walkthrough-controls">
+            <button className="walkthrough-control-button"><i className="fa fa-arrow-left" /></button>
             <span>2. Querying Vega</span>
-            <button class="walkthrough-control-button"><i class="fa fa-arrow-right" /></button>
+            <button className="walkthrough-control-button"><i className="fa fa-arrow-right" /></button>
           </div>
-          Type some GraphQL and see the output.
+          <ReactMarkdown>
+            {markdown}
+          </ReactMarkdown>
         </div>
-        <div class="walkthrough-panel walkthrough-panels-io">
-          <div class="walkthrough-panels-input">
-            <CodeMirror value={value}
-                options={options}
+        <div className="walkthrough-panel walkthrough-panels-io">
+          <div className="walkthrough-panels-input">
+            <div className="walkthrough-codemirror-container">
+              <CodeMirror 
+                className="walkthrough-codemirror"
+                value={value}
+                options={editorOptions}
                 onBeforeChange={(editor, data, value) => {
                   setValue(value);
                 }}
                 onChange={(editor, data, value) => {
                 }}
               />
-          </div>
-          <div class="walkthrough-panels-output">
+            </div>
+         </div>
+          <div className="walkthrough-panels-output">
             This is the output.
           </div>
         </div>
---
+++ GraphQL-Guides/apps/vega-walkthrough/src/markdown/introduction.md
@@ -0,0 +1,17 @@
+### Introduction
+
+The script you see on the right is GraphQL.
+
+GraphQL is a concise language for making queries.
+
+Vega Protocol is a decentralized derivatives trading platform.
+
+Vega provides APIs to allow you to interact with the system in a flexible way. In this guide we walk through using the GraphQL API. 
+
+You can use GraphQL very aspect of Vega Protocol. So you have great power.
+
+#### Assets 
+
+Let's get up and running with a simple example. We see a query that gets the assets available on Vega Protocol and returns their name, symbol and totalSupply.
+
+Click run to view the results.
